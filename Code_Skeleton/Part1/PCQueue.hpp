eee21a2009b738496e78f3578357120cf7a983b3
ofir <ofirflorentz@campus.technion.ac.il>
2020-12-28 09:15:05 +0000

    first commit

--- Code_Skeleton/Part1/PCQueue.hpp
+++ Code_Skeleton/Part1/PCQueue.hpp
@@ -1,6 +1,6 @@
 #ifndef _QUEUEL_H
 #define _QUEUEL_H
-#include "Headers.h"
+#include "Headers.hpp"
 // Single Producer - Multiple Consumer queue
 template <typename T>class PCQueue
 {
---
+++ Code_Skeleton/Part1/Semaphore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by student on 12/27/20.
+//
+
+#include "Semaphore.hpp"
