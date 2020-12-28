3bc37a619cab03068b694999b9a8ec9b31180a89
student <shachar373@gmail.com>
2020-12-28 09:16:17 +0000

    sfs

--- Code_Skeleton/Part1/Semaphore.cpp
+++ Code_Skeleton/Part1/Semaphore.cpp
@@ -1,5 +1,33 @@
 //
 // Created by student on 12/27/20.
 //
-
 #include "Semaphore.hpp"
+
+Semaphore::Semaphore() : m(), c(), counter(1), min(0), next(0) {};
+
+Semaphore::Semaphore(unsigned val) : m(), c(), counter(val), min(0), next(0) { // TODO val=0 (piazza?)
+    if (val == 0) { // TODO
+        counter = 1;
+    }
+};
+
+void Semaphore::up() {
+    pthread_mutex_lock(&m);
+    ++counter;
+    pthread_cond_broadcast(&c);
+//    pthread_cond_signal(&c); // TODO
+    pthread_mutex_unlock(&m);
+}
+
+void Semaphore::down() {
+    pthread_mutex_lock(&m);
+
+    unsigned int id = next++;
+
+    while (id >= counter+min || counter == 0) {
+        pthread_cond_wait(&c, &m);
+    }
+    --counter;
+    ++min;
+    pthread_mutex_unlock(&m);
+}
--- Code_Skeleton/Part1/Semaphore.hpp
+++ Code_Skeleton/Part1/Semaphore.hpp
@@ -5,15 +5,19 @@
 // Synchronization Warm up 
 class Semaphore {
 public:
-	Semaphore(); // Constructs a new semaphore with a counter of 0
-	Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
+    Semaphore(); // Constructs a new semaphore with a counter of 0  // TODO nonsense
+    Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
 
 
-	void up(); // Mark: 1 Thread has left the critical section
-	void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
+    void up(); // Mark: 1 Thread has left the critical section
+    void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
 
 private:
-	// TODO 
+    pthread_mutex_t m;
+    pthread_cond_t c;
+    unsigned int counter;
+    unsigned int min;
+    unsigned int next;
 };
 
 #endif
