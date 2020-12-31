285cdbe5134f60c62343544466d35a376c8177c4
student <shachar373@gmail.com>
2020-12-31 15:58:39 +0000

    trying to test ThreadP-thread_workload. right now the works is done but the board does not change at all (V3)

--- selfTests/t4.cpp
+++ selfTests/t4.cpp
@@ -28,8 +28,8 @@
 
 void test1 () {
     cout << "************ start test1 ************" << endl;
-    vector<int> tmp1 = {1,0,0,3,0,0,0,0};
-    vector<int> tmp2 = {1,0,1,0,0,0,0,2};
+    vector<int> tmp1 = {1,2,0,3,0,0,0,0};
+    vector<int> tmp2 = {1,3,1,0,0,0,0,2};
     vector<int> tmp3 = {0,0,0,3,0,0,0,0};
     vector<int> tmp4 = {0,0,0,5,7,0,0,1};
     vector<int> tmp5 = {0,0,6,5,5,0,0,0};
@@ -78,8 +78,8 @@ void test2 () {
 void test3 () {
     cout << "************ start test3 ************" << endl;
     vector<int> tmp1 = {1,1,1,1,1,1,3,3};
-    vector<int> tmp2 = {1,1,1,0,0,1,3,3};
-    vector<int> tmp3 = {1,1,1,0,0,1,1,1};
+    vector<int> tmp2 = {1,1,0,0,0,1,3,3};
+    vector<int> tmp3 = {1,1,0,0,0,1,1,1};
     vector<int> tmp4 = {2,2,1,1,1,1,1,1};
     vector<int> tmp5 = {2,2,1,1,1,1,1,1};
 
@@ -100,14 +100,37 @@ void test3 () {
     cout << "************ finish test3 ************\n" << endl;
 }
 
+void test4 () {
+    cout << "************ start test4 ************" << endl;
+    vector<int> tmp1 = {1,1,1,0,1,0,1,1};
+    vector<int> tmp2 = {0,1,0,1,0,1,0,1};
+    vector<int> tmp3 = {0,1,0,2,0,3,0,1};
+    vector<int> tmp4 = {0,7,0,1,0,2,0,1};
+    vector<int> tmp5 = {7,0,1,1,2,0,1,1};
+
+
+    vector<vector<int>> board1 = {tmp1, tmp2, tmp3, tmp4, tmp5};
+    vector<vector<int>> next_board = board1;
+
+    ThreadP* t = new ThreadP(0,board1,next_board,0,5);
+    t->start();
+    t->join();
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            cout << next_board[i][j];
+        }
+        cout << endl;
+    }
+    cout << "************ finish test4 ************\n" << endl;
+}
+
 
 int main() {
     test1();
     test2();
     test3();
-
-
-
+    test4();
 
     return 0;
 }
