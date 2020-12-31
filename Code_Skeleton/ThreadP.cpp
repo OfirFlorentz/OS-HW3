e052382343021640ff00d25c1345ace33078831a
student <shachar373@gmail.com>
2020-12-31 15:06:59 +0000

    wrote ThreadP (wrote thread_workload)

--- Code_Skeleton/ThreadP.cpp
+++ Code_Skeleton/ThreadP.cpp
@@ -5,8 +5,54 @@
 #include "Thread.hpp"
 
 
-ThreadP::ThreadP(uint thread_id) : Thread(thread_id){}
+ThreadP::ThreadP(uint thread_id, vector<vector<int>> & curr_board, vector<vector<int>> & next_board, int start_line,
+                 int num_of_line, int time) : Thread(thread_id), curr_board(curr_board), next_board(next_board),
+                 start_line(start_line), num_of_line(num_of_line), time(0) {};
 
 void ThreadP::thread_workload() {
-    cout << "works for number " << m_thread_id << " thread" << endl;
+    cout << "start the work of number thread " << m_thread_id << endl;
+    auto start = std::chrono::system_clock::now();
+
+    for (int i = start_line; i < start_line+num_of_line; ++i) {
+        for (int j = 0; j < curr_board[start_line].size(); ++j) {
+            uint bottom = 0 > i-1 ? 0 : i-1;
+            uint top = curr_board.size()-1 < i+1 ? curr_board.size()-1 : i+1;
+            uint left = 0 > j-1 ? 0 : j-1;
+            uint right = curr_board[0].size()-1 < j+1 ? curr_board[0].size()-1 : j+1;
+
+            int num_of_neighbours = 0;
+            vector<int> colors_hist = {0,0,0,0,0,0,0,0};
+            for (int k = bottom; k <= top; ++k) { // count num of alive neighbours
+                for (int l = left; l < right; ++l) {
+                    if ((k != i && l != j) && curr_board[k][l] != 0) {
+                        if (++num_of_neighbours <= 3) {
+                            colors_hist[(int)(curr_board[k][l])]++; // mapping dominant species of neighbours
+                        }
+                    }
+                }
+            }
+
+            if (curr_board[i][j] == 0 && num_of_neighbours == 3) {
+                int dominant_species = 0;
+                int max = 0;
+                for (int k = 0; k < colors_hist.size(); ++k) {
+                    if (colors_hist[k] > max) {
+                        max = colors_hist[k];
+                        dominant_species = k;
+                    }
+                next_board[i][j] = dominant_species;
+                }
+            }
+            else if (curr_board[i][j] != 0 && num_of_neighbours != 2 && num_of_neighbours != 3) {
+                    next_board[i][j] = 0;
+            }
+        }
+    }
+
+    auto end = std::chrono::system_clock::now();
+    time = start-end;
+}
+
+std::chrono::duration<double> ThreadP::get_iter_time() {
+    return time;
 }
